08eefc378c537fdfe351abf68064906584d29897
imp <zhangyifan@papegames.net>
2022-01-24 15:59:39 +0800

    feat: Added layers and layer stack.
    Signed-off-by: imp <zhangyifan@papegames.net>

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -6,37 +6,56 @@ namespace Hazel
 {
 #define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
 
-Application::Application() {
-	m_Window = std::unique_ptr<Window>(Window::Create());
-	
-	m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
-}
+  Application::Application() {
+    m_Window = std::unique_ptr<Window>(Window::Create());
 
-Application::~Application() {}
+    m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
+  }
 
-void Application::OnEvent(Event& e)
-{
-	HZ_HAZEL_TRACE("{0}", e);
-	
-	EventDispatcher dispather(e);
-	
-	dispather.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
-}
+  Application::~Application() {}
 
-void Application::Run()
-{
-	while (m_Running) {
-		glClearColor(0.4f, 0.3f, 0.4f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		m_Window->OnUpdate();
-	}
-}
+  void Application::PushLayer(Layer* layer)
+  {
+    m_LayerStack.PushLayer(layer);
+  }
 
-bool Application::OnWindowClose(WindowCloseEvent& e)
-{
-	m_Running = false;
-	
-	return true;
-};
+  void Application::PushOverlay(Layer* overlay)
+  {
+    m_LayerStack.PushOverlay(overlay);
+  }
+
+  void Application::OnEvent(Event& e)
+  {
+    EventDispatcher dispather(e);
+    dispather.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+
+    for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();)
+    {
+      // *it 就是迭代器it指向的元素 这里做 --it 运算 进入下一个迭代
+      (*--it)->OnEvent(e);
+      if (e.Handled)
+        break;
+    }
+  }
+
+  void Application::Run()
+  {
+    while (m_Running) {
+      glClearColor(0.4f, 0.3f, 0.4f, 0.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      for (Layer* layer : m_LayerStack)
+        layer->OnUpdate();
+
+      m_Window->OnUpdate();
+    }
+  }
+
+  bool Application::OnWindowClose(WindowCloseEvent& e)
+  {
+    m_Running = false;
+
+    return true;
+  };
 
 }
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,10 +1,8 @@
 #pragma once
 
-//#include "Core.h"
-//#include "Events/Event.h"
-#include "Events/ApplicationEvent.h"
-
 #include "Window.h"
+#include "LayerStack.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Hazel
 {
@@ -18,11 +16,15 @@ namespace Hazel
     void Run();
 		
 		void OnEvent(Event& e);
+
+    void PushLayer(Layer* layer);
+    void PushOverlay(Layer* overlay);
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 		
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+    LayerStack m_LayerStack;
   };
 
   // To be defined in CLIENT
