a1b5703308cb00d4e44b71b909e25388c8f5526b
imp <zhangyifan@papegames.net>
2022-01-25 22:43:15 +0800

    fix: imgui dpi
    Signed-off-by: imp <zhangyifan@papegames.net>

--- Hazel/src/ImGui/ImGuiLayer.cpp
+++ Hazel/src/ImGui/ImGuiLayer.cpp
@@ -2,6 +2,7 @@
 
 #include "imgui.h"
 #include "Platform/OpenGL/ImGuiOpenGLRenderer.h"
+#include "imgui_impl_glfw.h"
 
 #include "Hazel/Application.h"
 
@@ -10,273 +11,84 @@
 
 namespace Hazel {
 
-  static ImGuiKey ImGui_ImplGlfw_KeyToImGuiKey(int key)
-  {
-    switch (key)
-    {
-    case GLFW_KEY_TAB: return ImGuiKey_Tab;
-    case GLFW_KEY_LEFT: return ImGuiKey_LeftArrow;
-    case GLFW_KEY_RIGHT: return ImGuiKey_RightArrow;
-    case GLFW_KEY_UP: return ImGuiKey_UpArrow;
-    case GLFW_KEY_DOWN: return ImGuiKey_DownArrow;
-    case GLFW_KEY_PAGE_UP: return ImGuiKey_PageUp;
-    case GLFW_KEY_PAGE_DOWN: return ImGuiKey_PageDown;
-    case GLFW_KEY_HOME: return ImGuiKey_Home;
-    case GLFW_KEY_END: return ImGuiKey_End;
-    case GLFW_KEY_INSERT: return ImGuiKey_Insert;
-    case GLFW_KEY_DELETE: return ImGuiKey_Delete;
-    case GLFW_KEY_BACKSPACE: return ImGuiKey_Backspace;
-    case GLFW_KEY_SPACE: return ImGuiKey_Space;
-    case GLFW_KEY_ENTER: return ImGuiKey_Enter;
-    case GLFW_KEY_ESCAPE: return ImGuiKey_Escape;
-    case GLFW_KEY_APOSTROPHE: return ImGuiKey_Apostrophe;
-    case GLFW_KEY_COMMA: return ImGuiKey_Comma;
-    case GLFW_KEY_MINUS: return ImGuiKey_Minus;
-    case GLFW_KEY_PERIOD: return ImGuiKey_Period;
-    case GLFW_KEY_SLASH: return ImGuiKey_Slash;
-    case GLFW_KEY_SEMICOLON: return ImGuiKey_Semicolon;
-    case GLFW_KEY_EQUAL: return ImGuiKey_Equal;
-    case GLFW_KEY_LEFT_BRACKET: return ImGuiKey_LeftBracket;
-    case GLFW_KEY_BACKSLASH: return ImGuiKey_Backslash;
-    case GLFW_KEY_RIGHT_BRACKET: return ImGuiKey_RightBracket;
-    case GLFW_KEY_GRAVE_ACCENT: return ImGuiKey_GraveAccent;
-    case GLFW_KEY_CAPS_LOCK: return ImGuiKey_CapsLock;
-    case GLFW_KEY_SCROLL_LOCK: return ImGuiKey_ScrollLock;
-    case GLFW_KEY_NUM_LOCK: return ImGuiKey_NumLock;
-    case GLFW_KEY_PRINT_SCREEN: return ImGuiKey_PrintScreen;
-    case GLFW_KEY_PAUSE: return ImGuiKey_Pause;
-    case GLFW_KEY_KP_0: return ImGuiKey_Keypad0;
-    case GLFW_KEY_KP_1: return ImGuiKey_Keypad1;
-    case GLFW_KEY_KP_2: return ImGuiKey_Keypad2;
-    case GLFW_KEY_KP_3: return ImGuiKey_Keypad3;
-    case GLFW_KEY_KP_4: return ImGuiKey_Keypad4;
-    case GLFW_KEY_KP_5: return ImGuiKey_Keypad5;
-    case GLFW_KEY_KP_6: return ImGuiKey_Keypad6;
-    case GLFW_KEY_KP_7: return ImGuiKey_Keypad7;
-    case GLFW_KEY_KP_8: return ImGuiKey_Keypad8;
-    case GLFW_KEY_KP_9: return ImGuiKey_Keypad9;
-    case GLFW_KEY_KP_DECIMAL: return ImGuiKey_KeypadDecimal;
-    case GLFW_KEY_KP_DIVIDE: return ImGuiKey_KeypadDivide;
-    case GLFW_KEY_KP_MULTIPLY: return ImGuiKey_KeypadMultiply;
-    case GLFW_KEY_KP_SUBTRACT: return ImGuiKey_KeypadSubtract;
-    case GLFW_KEY_KP_ADD: return ImGuiKey_KeypadAdd;
-    case GLFW_KEY_KP_ENTER: return ImGuiKey_KeypadEnter;
-    case GLFW_KEY_KP_EQUAL: return ImGuiKey_KeypadEqual;
-    case GLFW_KEY_LEFT_SHIFT: return ImGuiKey_LeftShift;
-    case GLFW_KEY_LEFT_CONTROL: return ImGuiKey_LeftCtrl;
-    case GLFW_KEY_LEFT_ALT: return ImGuiKey_LeftAlt;
-    case GLFW_KEY_LEFT_SUPER: return ImGuiKey_LeftSuper;
-    case GLFW_KEY_RIGHT_SHIFT: return ImGuiKey_RightShift;
-    case GLFW_KEY_RIGHT_CONTROL: return ImGuiKey_RightCtrl;
-    case GLFW_KEY_RIGHT_ALT: return ImGuiKey_RightAlt;
-    case GLFW_KEY_RIGHT_SUPER: return ImGuiKey_RightSuper;
-    case GLFW_KEY_MENU: return ImGuiKey_Menu;
-    case GLFW_KEY_0: return ImGuiKey_0;
-    case GLFW_KEY_1: return ImGuiKey_1;
-    case GLFW_KEY_2: return ImGuiKey_2;
-    case GLFW_KEY_3: return ImGuiKey_3;
-    case GLFW_KEY_4: return ImGuiKey_4;
-    case GLFW_KEY_5: return ImGuiKey_5;
-    case GLFW_KEY_6: return ImGuiKey_6;
-    case GLFW_KEY_7: return ImGuiKey_7;
-    case GLFW_KEY_8: return ImGuiKey_8;
-    case GLFW_KEY_9: return ImGuiKey_9;
-    case GLFW_KEY_A: return ImGuiKey_A;
-    case GLFW_KEY_B: return ImGuiKey_B;
-    case GLFW_KEY_C: return ImGuiKey_C;
-    case GLFW_KEY_D: return ImGuiKey_D;
-    case GLFW_KEY_E: return ImGuiKey_E;
-    case GLFW_KEY_F: return ImGuiKey_F;
-    case GLFW_KEY_G: return ImGuiKey_G;
-    case GLFW_KEY_H: return ImGuiKey_H;
-    case GLFW_KEY_I: return ImGuiKey_I;
-    case GLFW_KEY_J: return ImGuiKey_J;
-    case GLFW_KEY_K: return ImGuiKey_K;
-    case GLFW_KEY_L: return ImGuiKey_L;
-    case GLFW_KEY_M: return ImGuiKey_M;
-    case GLFW_KEY_N: return ImGuiKey_N;
-    case GLFW_KEY_O: return ImGuiKey_O;
-    case GLFW_KEY_P: return ImGuiKey_P;
-    case GLFW_KEY_Q: return ImGuiKey_Q;
-    case GLFW_KEY_R: return ImGuiKey_R;
-    case GLFW_KEY_S: return ImGuiKey_S;
-    case GLFW_KEY_T: return ImGuiKey_T;
-    case GLFW_KEY_U: return ImGuiKey_U;
-    case GLFW_KEY_V: return ImGuiKey_V;
-    case GLFW_KEY_W: return ImGuiKey_W;
-    case GLFW_KEY_X: return ImGuiKey_X;
-    case GLFW_KEY_Y: return ImGuiKey_Y;
-    case GLFW_KEY_Z: return ImGuiKey_Z;
-    case GLFW_KEY_F1: return ImGuiKey_F1;
-    case GLFW_KEY_F2: return ImGuiKey_F2;
-    case GLFW_KEY_F3: return ImGuiKey_F3;
-    case GLFW_KEY_F4: return ImGuiKey_F4;
-    case GLFW_KEY_F5: return ImGuiKey_F5;
-    case GLFW_KEY_F6: return ImGuiKey_F6;
-    case GLFW_KEY_F7: return ImGuiKey_F7;
-    case GLFW_KEY_F8: return ImGuiKey_F8;
-    case GLFW_KEY_F9: return ImGuiKey_F9;
-    case GLFW_KEY_F10: return ImGuiKey_F10;
-    case GLFW_KEY_F11: return ImGuiKey_F11;
-    case GLFW_KEY_F12: return ImGuiKey_F12;
-    default: return ImGuiKey_None;
-    }
-  }
+	ImGuiLayer::ImGuiLayer()
+		: Layer("ImGuiLayer")
+	{
+	}
 
-  static int ImGui_ImplGlfw_TranslateUntranslatedKey(int key, int scancode)
-  {
-#if GLFW_HAS_GET_KEY_NAME && !defined(__EMSCRIPTEN__)
-    // GLFW 3.1+ attempts to "untranslate" keys, which goes the opposite of what every other framework does, making using lettered shortcuts difficult.
-    // (It had reasons to do so: namely GLFW is/was more likely to be used for WASD-type game controls rather than lettered shortcuts, but IHMO the 3.1 change could have been done differently)
-    // See https://github.com/glfw/glfw/issues/1502 for details.
-    // Adding a workaround to undo this (so our keys are translated->untranslated->translated, likely a lossy process).
-    // This won't cover edge cases but this is at least going to cover common cases.
-    const char* key_name = glfwGetKeyName(key, scancode);
-    if (key_name && key_name[0] != 0 && key_name[1] == 0)
-    {
-      const char char_names[] = "`-=[]\\,;\'./";
-      const int char_keys[] = { GLFW_KEY_GRAVE_ACCENT, GLFW_KEY_MINUS, GLFW_KEY_EQUAL, GLFW_KEY_LEFT_BRACKET, GLFW_KEY_RIGHT_BRACKET, GLFW_KEY_BACKSLASH, GLFW_KEY_COMMA, GLFW_KEY_SEMICOLON, GLFW_KEY_APOSTROPHE, GLFW_KEY_PERIOD, GLFW_KEY_SLASH, 0 };
-      IM_ASSERT(IM_ARRAYSIZE(char_names) == IM_ARRAYSIZE(char_keys));
-      if (key_name[0] >= '0' && key_name[0] <= '9') { key = GLFW_KEY_0 + (key_name[0] - '0'); }
-      else if (key_name[0] >= 'A' && key_name[0] <= 'Z') { key = GLFW_KEY_A + (key_name[0] - 'A'); }
-      else if (const char* p = strchr(char_names, key_name[0])) { key = char_keys[p - char_names]; }
-    }
-    // if (action == GLFW_PRESS) printf("key %d scancode %d name '%s'\n", key, scancode, key_name);
-#else
-    IM_UNUSED(scancode);
-#endif
+	ImGuiLayer::~ImGuiLayer()
+	{
+	}
 
-    return key;
-  }
+	void ImGuiLayer::OnAttach()
+	{
+		#if defined(IMGUI_IMPL_OPENGL_ES2)
+				// GL ES 2.0 + GLSL 100
+				const char* glsl_version = "#version 100";
+				glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+				glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+				glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+		#elif defined(__APPLE__)
+				// GL 3.2 + GLSL 150
+				const char* glsl_version = "#version 150";
+				glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+				glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+				glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+				glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+		#else
+				// GL 3.0 + GLSL 130
+				const char* glsl_version = "#version 130";
+				glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+				glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+				//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+				//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+		#endif
+		
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
 
-  ImGuiLayer::ImGuiLayer()
-    : Layer("ImGuiLayer")
-  {
-  }
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		
+		Application& app = Application::Get();
+		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
 
-  ImGuiLayer::~ImGuiLayer()
-  {
-  }
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init(glsl_version);
+	}
 
-  void ImGuiLayer::OnAttach()
-  {
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
+	void ImGuiLayer::OnDetach()
+	{
+	}
 
-    ImGuiIO& io = ImGui::GetIO();
-    io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
-    io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
+	void ImGuiLayer::OnUpdate()
+	{
+//    ImGuiIO& io = ImGui::GetIO();
+//    Application& app = Application::Get();
+//    io.DisplaySize = ImVec2(app.GetWindow().GetWidth(), app.GetWindow().GetHeight());
+//
+//    float time = (float)glfwGetTime();
+//    io.DeltaTime = m_Time > 0.0f ? (time - m_Time) : (1.0f / 60.0f);
+//    m_Time = time;
 
-    ImGui_ImplOpenGL3_Init("#version 410");
-  }
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
 
-  void ImGuiLayer::OnDetach()
-  {
-  }
+		static bool show = true;
+		ImGui::ShowDemoWindow(&show);
 
-  void ImGuiLayer::OnUpdate()
-  {
-    ImGuiIO& io = ImGui::GetIO();
-    Application& app = Application::Get();
-    io.DisplaySize = ImVec2(app.GetWindow().GetWidth(), app.GetWindow().GetHeight());
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	}
 
-    float time = (float)glfwGetTime();
-    io.DeltaTime = m_Time > 0.0f ? (time - m_Time) : (1.0f / 60.0f);
-    m_Time = time;
-
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui::NewFrame();
-
-    static bool show = true;
-    ImGui::ShowDemoWindow(&show);
-
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-  }
-
-  void ImGuiLayer::OnEvent(Event& event)
-  {
-    EventDispatcher dispatcher(event);
-    dispatcher.Dispatch<MouseButtonPressedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonPressedEvent));
-    dispatcher.Dispatch<MouseButtonReleasedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonReleasedEvent));
-    dispatcher.Dispatch<MouseMovedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseMovedEvent));
-    dispatcher.Dispatch<MouseScrolledEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseScrolledEvent));
-    dispatcher.Dispatch<KeyPressedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnKeyPressedEvent));
-    dispatcher.Dispatch<KeyTypedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnKeyTypedEvent));
-    dispatcher.Dispatch<KeyReleasedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnKeyReleasedEvent));
-    dispatcher.Dispatch<WindowResizeEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnWindowResizeEvent));
-  }
-
-  bool ImGuiLayer::OnMouseButtonPressedEvent(MouseButtonPressedEvent& e)
-  {
-    ImGuiIO& io = ImGui::GetIO();
-    io.AddMouseButtonEvent(e.GetMouseButton(), true);
-
-    return false;
-  }
-
-  bool ImGuiLayer::OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e)
-  {
-    ImGuiIO& io = ImGui::GetIO();
-    io.AddMouseButtonEvent(e.GetMouseButton(), false);
-
-    return false;
-  }
-
-  bool ImGuiLayer::OnMouseMovedEvent(MouseMovedEvent& e)
-  {
-    ImGuiIO& io = ImGui::GetIO();
-    io.AddMousePosEvent(e.GetX(), e.GetY());
-
-    return false;
-  }
-
-  bool ImGuiLayer::OnMouseScrolledEvent(MouseScrolledEvent& e)
-  {
-    ImGuiIO& io = ImGui::GetIO();
-    io.AddMouseWheelEvent(e.GetXOffset(), e.GetYOffset());
-
-    return false;
-  }
-
-  bool ImGuiLayer::OnKeyPressedEvent(KeyPressedEvent& e)
-  {
-    int keycode = ImGui_ImplGlfw_TranslateUntranslatedKey(e.GetKeyCode(), e.GetKeyGetScanCode());
-
-    ImGuiIO& io = ImGui::GetIO();
-    ImGuiKey imgui_key = ImGui_ImplGlfw_KeyToImGuiKey(keycode);
-    io.AddKeyEvent(imgui_key, true);
-
-    return false;
-  }
-
-  bool ImGuiLayer::OnKeyReleasedEvent(KeyReleasedEvent& e)
-  {
-    int keycode = ImGui_ImplGlfw_TranslateUntranslatedKey(e.GetKeyCode(), e.GetKeyGetScanCode());
-
-    ImGuiIO& io = ImGui::GetIO();
-    ImGuiKey imgui_key = ImGui_ImplGlfw_KeyToImGuiKey(keycode);
-    io.AddKeyEvent(imgui_key, false);
-
-    return false;
-  }
-
-  bool ImGuiLayer::OnKeyTypedEvent(KeyTypedEvent& e)
-  {
-    ImGuiIO& io = ImGui::GetIO();
-    io.AddInputCharacter((unsigned int)e.GetKeyCode());
-
-    return false;
-  }
-
-  bool ImGuiLayer::OnWindowResizeEvent(WindowResizeEvent& e)
-  {
-    ImGuiIO& io = ImGui::GetIO();
-    io.DisplaySize = ImVec2(e.GetWidth(), e.GetHeight());
-    io.DisplayFramebufferScale = ImVec2(1.0f, 1.0f);
-    glViewport(0, 0, e.GetWidth(), e.GetHeight());
-
-    return false;
-  }
+	void ImGuiLayer::OnEvent(Event& event)
+	{
+		if (m_BlockEvents)
+		{
+			ImGuiIO& io = ImGui::GetIO();
+			event.Handled |= event.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
+			event.Handled |= event.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
+		}
+	}
 
 }
--- Hazel/src/ImGui/ImGuiLayer.h
+++ Hazel/src/ImGui/ImGuiLayer.h
@@ -19,18 +19,11 @@ namespace Hazel {
     void OnDetach();
     void OnUpdate();
     void OnEvent(Event& event);
-
+		
+		inline void BlockEvents(bool block) { m_BlockEvents = block; }
+		
   private:
-    bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
-    bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
-    bool OnMouseMovedEvent(MouseMovedEvent& e);
-    bool OnMouseScrolledEvent(MouseScrolledEvent& e);
-    bool OnKeyPressedEvent(KeyPressedEvent& e);
-    bool OnKeyReleasedEvent(KeyReleasedEvent& e);
-    bool OnKeyTypedEvent(KeyTypedEvent& e);
-    bool OnWindowResizeEvent(WindowResizeEvent& e);
-
-  private:
+		bool m_BlockEvents = true;
     float m_Time = 0.0f;
   };
 
