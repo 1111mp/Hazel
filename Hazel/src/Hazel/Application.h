36fe989380c03cd42498c3f4a73d90a2fb5fe297
imp <zhangyifan@papegames.net>
2022-01-28 14:16:39 +0800

    feat: Our first triangle & Our first shader & Added OpenGL buffer abstraction.
    Signed-off-by: imp <zhangyifan@papegames.net>

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -6,6 +6,9 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Renderer/Shader.h"
+#include "Renderer/Buffer.h"
+
 namespace Hazel
 {
 
@@ -32,6 +35,10 @@ namespace Hazel
 		bool m_Running = true;
     LayerStack m_LayerStack;
 
+    unsigned int m_VertexArray;
+    std::unique_ptr<Shader> m_Shader;
+    std::unique_ptr<VertexBuffer> m_VertexBuffer;
+    std::unique_ptr<IndexBuffer> m_IndexBuffer;
   private:
     static Application* s_Instance;
   };
