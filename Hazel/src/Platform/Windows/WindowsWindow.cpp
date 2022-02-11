f9d6fe227db9256494a0635e0bdfb11544f69ce8
imp <zhangyifan@papegames.net>
2022-02-11 15:17:15 +0800

    feat: Added transforms and textures to Renderer2D.

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -8,7 +8,7 @@
 
 namespace Hazel {
 
-  static bool s_GLFWInitialized = false;
+  static uint8_t s_GLFWWindowCount = 0;
 
   static void GLFWErrorCallback(int error_code, const char* description)
   {
@@ -38,8 +38,9 @@ namespace Hazel {
 
     HZ_HAZEL_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
 
-    if (!s_GLFWInitialized)
+    if (s_GLFWWindowCount == 0)
     {
+      HZ_HAZEL_INFO("Initializing GLFW");
       // TODO: glfwTerminate on system shutdown
       int success = glfwInit();
       HZ_CORE_ASSERT(success, "Could not intialize GLFW!");
@@ -67,11 +68,10 @@ namespace Hazel {
 #endif
 
       glfwSetErrorCallback(GLFWErrorCallback);
-
-      s_GLFWInitialized = true;
     }
 
     m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
+    ++s_GLFWWindowCount;
 
     m_Context = CreateScope<OpenGLContext>(m_Window);
     m_Context->Init();
@@ -169,6 +169,11 @@ namespace Hazel {
   void WindowsWindow::Shutdown()
   {
     glfwDestroyWindow(m_Window);
+    if (--s_GLFWWindowCount = 0)
+    {
+      HZ_HAZEL_INFO("Terminating GLFW");
+      glfwTerminate();
+    }
   }
 
   void WindowsWindow::OnUpdate()
