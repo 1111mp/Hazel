6b0cee7bbc0472d2666da0d0206fac7b4e43f7f7
imp <zhangyifan@papegames.net>
2022-02-18 18:32:02 +0800

    feat: Add sprite texture.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -16,6 +16,10 @@ void Sandbox2D::OnAttach()
   HZ_PROFILE_FUNCTION();
 
   m_CheckerboardTexture = Hazel::Texture2D::Create(AssetsDir + "/assets/textures/Checkerboard.png");
+  m_SpriteSheet = Hazel::Texture2D::Create(AssetsDir + "/assets/game/textures/RPGpack_sheet_2X.png");
+
+  m_TextureStairs = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 7, 6 }, { 128, 128 });
+  m_TextureTree = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 2, 1 }, { 128, 128 }, { 1, 2 });
 
   m_Particle.ColorBegin = { 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
   m_Particle.ColorEnd = { 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 1.0f };
@@ -46,19 +50,21 @@ void Sandbox2D::OnUpdate(Hazel::TimeStep ts)
     Hazel::RenderCommand::Clear();
   }
 
-  {
-    static float rotation = 0.0f;
-    rotation += ts * 50.0f;
+  #if 0
+    {
+      static float rotation = 0.0f;
+      rotation += ts * 50.0f;
 
-    HZ_PROFILE_SCOPE("Renderer Draw");
-    Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
-    Hazel::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
-    Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-    Hazel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-    Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
-    Hazel::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, glm::radians(rotation), m_CheckerboardTexture, 20.0f);
-    Hazel::Renderer2D::EndScene();
-  }
+      HZ_PROFILE_SCOPE("Renderer Draw");
+      Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+      Hazel::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+      Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+      Hazel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+      Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+      Hazel::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, glm::radians(rotation), m_CheckerboardTexture, 20.0f);
+      Hazel::Renderer2D::EndScene();
+    }
+  #endif
 
   if (Hazel::Input::IsMouseButtonPressed(HZ_MOUSE_BUTTON_LEFT))
   {
@@ -73,6 +79,13 @@ void Sandbox2D::OnUpdate(Hazel::TimeStep ts)
     m_Particle.Position = { x + pos.x, y + pos.y };
     for (int i = 0; i < 5; i++)
       m_ParticleSystem.Emit(m_Particle);
+  }
+
+  {
+    Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+    Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_TextureStairs);
+    Hazel::Renderer2D::DrawQuad({ 1.0f, 0.0f, 0.0f }, { 1.0f, 2.0f }, m_TextureTree);
+    Hazel::Renderer2D::EndScene();
   }
 
   m_ParticleSystem.OnUpdate(ts);
