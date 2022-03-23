f07547db89f7426b7ce332d4ee9f4a345a54c8ae
imp <zhangyifan@papegames.net>
2022-03-23 15:44:39 +0800

    feat: some submits from Hazel.
    Signed-off-by: imp <zhangyifan@papegames.net>

--- Hazel/src/Platform/OpenGL/OpenGLShader.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLShader.cpp
@@ -3,9 +3,10 @@
 #include <fstream>
 #include <glm/gtc/type_ptr.hpp>
 
-namespace Hazel {
+namespace Hazel
+{
 
-  static GLenum ShaderTypeFromString(const std::string& type)
+  static GLenum ShaderTypeFromString(const std::string &type)
   {
     if (type == "vertex")
       return GL_VERTEX_SHADER;
@@ -16,7 +17,7 @@ namespace Hazel {
     return 0;
   }
 
-  OpenGLShader::OpenGLShader(const std::string& filepath)
+  OpenGLShader::OpenGLShader(const std::string &filepath)
   {
     HZ_PROFILE_FUNCTION();
 
@@ -32,8 +33,8 @@ namespace Hazel {
     m_Name = filepath.substr(lastSlash, count);
   }
 
-  OpenGLShader::OpenGLShader(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc)
-    : m_Name(name)
+  OpenGLShader::OpenGLShader(const std::string &name, const std::string &vertexSrc, const std::string &fragmentSrc)
+      : m_Name(name)
   {
     HZ_PROFILE_FUNCTION();
 
@@ -64,95 +65,102 @@ namespace Hazel {
     glUseProgram(0);
   }
 
-  void OpenGLShader::SetInt(const std::string& name, int value)
+  void OpenGLShader::SetInt(const std::string &name, int value)
   {
     HZ_PROFILE_FUNCTION();
 
     UploadUniformInt(name, value);
   }
 
-  void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count)
+  void OpenGLShader::SetIntArray(const std::string &name, int *values, uint32_t count)
   {
     UploadUniformIntArray(name, values, count);
   }
 
-  void OpenGLShader::SetFloat(const std::string& name, float value)
+  void OpenGLShader::SetFloat(const std::string &name, float value)
   {
     HZ_PROFILE_FUNCTION();
 
     UploadUniformFloat(name, value);
   }
 
-  void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
+  void OpenGLShader::SetFloat2(const std::string &name, const glm::vec2 &value)
+  {
+    HZ_PROFILE_FUNCTION();
+
+    UploadUniformFloat2(name, value);
+  }
+
+  void OpenGLShader::SetFloat3(const std::string &name, const glm::vec3 &value)
   {
     HZ_PROFILE_FUNCTION();
 
     UploadUniformFloat3(name, value);
   }
 
-  void OpenGLShader::SetFloat4(const std::string& name, const glm::vec4& value)
+  void OpenGLShader::SetFloat4(const std::string &name, const glm::vec4 &value)
   {
     HZ_PROFILE_FUNCTION();
 
     UploadUniformFloat4(name, value);
   }
 
-  void OpenGLShader::SetMat4(const std::string& name, const glm::mat4& value)
+  void OpenGLShader::SetMat4(const std::string &name, const glm::mat4 &value)
   {
     HZ_PROFILE_FUNCTION();
 
     UploadUniformMat4(name, value);
   }
 
-  void OpenGLShader::UploadUniformInt(const std::string& name, int value)
+  void OpenGLShader::UploadUniformInt(const std::string &name, int value)
   {
     GLint location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform1i(location, value);
   }
 
-  void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count)
+  void OpenGLShader::UploadUniformIntArray(const std::string &name, int *values, uint32_t count)
   {
     GLint location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform1iv(location, count, values);
   }
 
-  void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
+  void OpenGLShader::UploadUniformFloat(const std::string &name, float value)
   {
     GLint location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform1f(location, value);
   }
 
-  void OpenGLShader::UploadUniformFloat2(const std::string& name, const glm::vec2& value)
+  void OpenGLShader::UploadUniformFloat2(const std::string &name, const glm::vec2 &value)
   {
     GLint location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform2f(location, value.x, value.y);
   }
 
-  void OpenGLShader::UploadUniformFloat3(const std::string& name, const glm::vec3& value)
+  void OpenGLShader::UploadUniformFloat3(const std::string &name, const glm::vec3 &value)
   {
     GLint location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform3f(location, value.x, value.y, value.z);
   }
 
-  void OpenGLShader::UploadUniformFloat4(const std::string& name, const glm::vec4& value)
+  void OpenGLShader::UploadUniformFloat4(const std::string &name, const glm::vec4 &value)
   {
     GLint location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform4f(location, value.x, value.y, value.z, value.w);
   }
 
-  void OpenGLShader::UploadUniformMat3(const std::string& name, const glm::mat3& matrix)
+  void OpenGLShader::UploadUniformMat3(const std::string &name, const glm::mat3 &matrix)
   {
     GLint location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
   }
 
-  void OpenGLShader::UploadUniformMat4(const std::string& name, const glm::mat4& matrix)
+  void OpenGLShader::UploadUniformMat4(const std::string &name, const glm::mat4 &matrix)
   {
     GLint location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
   }
 
-  std::string OpenGLShader::ReadFile(const std::string& filepath)
+  std::string OpenGLShader::ReadFile(const std::string &filepath)
   {
     HZ_PROFILE_FUNCTION();
 
@@ -181,26 +189,26 @@ namespace Hazel {
     return result;
   }
 
-  std::unordered_map<GLenum, std::string> OpenGLShader::PreProcess(const std::string& source)
+  std::unordered_map<GLenum, std::string> OpenGLShader::PreProcess(const std::string &source)
   {
     HZ_PROFILE_FUNCTION();
 
     std::unordered_map<GLenum, std::string> shaderSources;
 
-    const char* typeToken = "#type";
+    const char *typeToken = "#type";
     size_t typeTokenLength = strlen(typeToken);
-    size_t pos = source.find(typeToken, 0); //Start of shader type declaration line
+    size_t pos = source.find(typeToken, 0); // Start of shader type declaration line
     while (pos != std::string::npos)
     {
-      size_t eol = source.find_first_of("\r\n", pos); //End of shader type declaration line
+      size_t eol = source.find_first_of("\r\n", pos); // End of shader type declaration line
       HZ_CORE_ASSERT(eol != std::string::npos, "Syntax error");
-      size_t begin = pos + typeTokenLength + 1; //Start of shader type name (after "#type " keyword)
+      size_t begin = pos + typeTokenLength + 1; // Start of shader type name (after "#type " keyword)
       std::string type = source.substr(begin, eol - begin);
       HZ_CORE_ASSERT(ShaderTypeFromString(type), "Invalid shader type specified");
 
-      size_t nextLinePos = source.find_first_not_of("\r\n", eol); //Start of shader code after shader type declaration line
+      size_t nextLinePos = source.find_first_not_of("\r\n", eol); // Start of shader code after shader type declaration line
       HZ_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
-      pos = source.find(typeToken, nextLinePos); //Start of next shader type declaration line
+      pos = source.find(typeToken, nextLinePos); // Start of next shader type declaration line
 
       shaderSources[ShaderTypeFromString(type)] = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
     }
@@ -208,7 +216,7 @@ namespace Hazel {
     return shaderSources;
   }
 
-  void OpenGLShader::Compile(std::unordered_map<GLenum, std::string>& shaderSources)
+  void OpenGLShader::Compile(std::unordered_map<GLenum, std::string> &shaderSources)
   {
     HZ_PROFILE_FUNCTION();
 
@@ -217,17 +225,17 @@ namespace Hazel {
     std::array<GLenum, 2> glShaderIDs;
     int glShaderIDIndex = 0;
 
-    for (auto& kv : shaderSources)
+    for (auto &kv : shaderSources)
     {
       GLenum type = kv.first;
-      const std::string& source = kv.second;
+      const std::string &source = kv.second;
 
       // Create an empty vertex shader handle
       GLuint shader = glCreateShader(type);
 
       // Send the vertex shader source code to GL
       // Note that std::string's .c_str is NULL character terminated.
-      const GLchar* sourceCStr = source.c_str();
+      const GLchar *sourceCStr = source.c_str();
       glShaderSource(shader, 1, &sourceCStr, 0);
 
       // Compile the vertex shader
@@ -266,7 +274,7 @@ namespace Hazel {
 
     // Note the different functions here: glGetProgram* instead of glGetShader*.
     GLint isLinked = 0;
-    glGetProgramiv(m_RendererID, GL_LINK_STATUS, (int*)&isLinked);
+    glGetProgramiv(m_RendererID, GL_LINK_STATUS, (int *)&isLinked);
     if (isLinked == GL_FALSE)
     {
       GLint maxLength = 0;
--- Hazel/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -2,122 +2,145 @@
 
 #include <glad/glad.h>
 
-namespace Hazel {
-
-static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+namespace Hazel
 {
-	switch (type)
-	{
-	case Hazel::ShaderDataType::Float:    return GL_FLOAT;
-	case Hazel::ShaderDataType::Float2:   return GL_FLOAT;
-	case Hazel::ShaderDataType::Float3:   return GL_FLOAT;
-	case Hazel::ShaderDataType::Float4:   return GL_FLOAT;
-	case Hazel::ShaderDataType::Mat3:     return GL_FLOAT;
-	case Hazel::ShaderDataType::Mat4:     return GL_FLOAT;
-	case Hazel::ShaderDataType::Int:      return GL_INT;
-	case Hazel::ShaderDataType::Int2:     return GL_INT;
-	case Hazel::ShaderDataType::Int3:     return GL_INT;
-	case Hazel::ShaderDataType::Int4:     return GL_INT;
-	case Hazel::ShaderDataType::Bool:     return GL_BOOL;
-	default:
-		HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
-		return 0;
-	}
-}
 
-OpenGLVertexArray::OpenGLVertexArray() {
-	HZ_PROFILE_FUNCTION();
-
-	glGenVertexArrays(1, &m_RendererID);
-}
-
-OpenGLVertexArray::~OpenGLVertexArray()
-{
-	HZ_PROFILE_FUNCTION();
-
-	glDeleteVertexArrays(1, &m_RendererID);
-}
-
-void OpenGLVertexArray::Bind() const {
-	glBindVertexArray(m_RendererID);
-}
-
-void OpenGLVertexArray::UnBind() const {
-	HZ_PROFILE_FUNCTION();
-
-	glBindVertexArray(0);
-}
-
-void OpenGLVertexArray::AddVertexBuffer(const Ref<VertexBuffer> &vertexBuffer)
-{
-	HZ_PROFILE_FUNCTION();
-
-	HZ_CORE_ASSERT(vertexBuffer->GetLayout().GetElements().size(), "Vertex Buffer has no layout!");
-	
-	glBindVertexArray(m_RendererID);
-	vertexBuffer->Bind();
-	
-  const auto& layout = vertexBuffer->GetLayout();
-	for (const auto& element : layout)
-	{
-    switch (element.Type)
+  static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+  {
+    switch (type)
     {
-    case ShaderDataType::Float:
-    case ShaderDataType::Float2:
-    case ShaderDataType::Float3:
-    case ShaderDataType::Float4:
-    case ShaderDataType::Int:
-    case ShaderDataType::Int2:
-    case ShaderDataType::Int3:
-    case ShaderDataType::Int4:
-    case ShaderDataType::Bool:
-    {
-      glEnableVertexAttribArray(m_VertexBufferIndex);
-      glVertexAttribPointer(m_VertexBufferIndex,
-        element.GetComponentCount(),
-        ShaderDataTypeToOpenGLBaseType(element.Type),
-        element.Normalized ? GL_TRUE : GL_FALSE,
-        layout.GetStride(),
-        (const void*)element.Offset
-      );
-      m_VertexBufferIndex++;
-      break;
+    case Hazel::ShaderDataType::Float:
+      return GL_FLOAT;
+    case Hazel::ShaderDataType::Float2:
+      return GL_FLOAT;
+    case Hazel::ShaderDataType::Float3:
+      return GL_FLOAT;
+    case Hazel::ShaderDataType::Float4:
+      return GL_FLOAT;
+    case Hazel::ShaderDataType::Mat3:
+      return GL_FLOAT;
+    case Hazel::ShaderDataType::Mat4:
+      return GL_FLOAT;
+    case Hazel::ShaderDataType::Int:
+      return GL_INT;
+    case Hazel::ShaderDataType::Int2:
+      return GL_INT;
+    case Hazel::ShaderDataType::Int3:
+      return GL_INT;
+    case Hazel::ShaderDataType::Int4:
+      return GL_INT;
+    case Hazel::ShaderDataType::Bool:
+      return GL_BOOL;
+    default:
+      HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+      return 0;
     }
-    case ShaderDataType::Mat3:
-    case ShaderDataType::Mat4:
+  }
+
+  OpenGLVertexArray::OpenGLVertexArray()
+  {
+    HZ_PROFILE_FUNCTION();
+
+    glGenVertexArrays(1, &m_RendererID);
+  }
+
+  OpenGLVertexArray::~OpenGLVertexArray()
+  {
+    HZ_PROFILE_FUNCTION();
+
+    glDeleteVertexArrays(1, &m_RendererID);
+  }
+
+  void OpenGLVertexArray::Bind() const
+  {
+    glBindVertexArray(m_RendererID);
+  }
+
+  void OpenGLVertexArray::UnBind() const
+  {
+    HZ_PROFILE_FUNCTION();
+
+    glBindVertexArray(0);
+  }
+
+  void OpenGLVertexArray::AddVertexBuffer(const Ref<VertexBuffer> &vertexBuffer)
+  {
+    HZ_PROFILE_FUNCTION();
+
+    HZ_CORE_ASSERT(vertexBuffer->GetLayout().GetElements().size(), "Vertex Buffer has no layout!");
+
+    glBindVertexArray(m_RendererID);
+    vertexBuffer->Bind();
+
+    const auto &layout = vertexBuffer->GetLayout();
+    for (const auto &element : layout)
     {
-      uint8_t count = element.GetComponentCount();
-      for (uint8_t i = 0; i < count; i++)
+      switch (element.Type)
+      {
+      case ShaderDataType::Float:
+      case ShaderDataType::Float2:
+      case ShaderDataType::Float3:
+      case ShaderDataType::Float4:
       {
         glEnableVertexAttribArray(m_VertexBufferIndex);
         glVertexAttribPointer(m_VertexBufferIndex,
-          count,
-          ShaderDataTypeToOpenGLBaseType(element.Type),
-          element.Normalized ? GL_TRUE : GL_FALSE,
-          layout.GetStride(),
-          (const void*)(element.Offset + sizeof(float) * count * i)
-        );
-        glVertexAttribDivisor(m_VertexBufferIndex, 1);
+                              element.GetComponentCount(),
+                              ShaderDataTypeToOpenGLBaseType(element.Type),
+                              element.Normalized ? GL_TRUE : GL_FALSE,
+                              layout.GetStride(),
+                              (const void *)element.Offset);
         m_VertexBufferIndex++;
+        break;
       }
-      break;
+      case ShaderDataType::Int:
+      case ShaderDataType::Int2:
+      case ShaderDataType::Int3:
+      case ShaderDataType::Int4:
+      case ShaderDataType::Bool:
+      {
+        glEnableVertexAttribArray(m_VertexBufferIndex);
+        glVertexAttribIPointer(m_VertexBufferIndex,
+                               element.GetComponentCount(),
+                               ShaderDataTypeToOpenGLBaseType(element.Type),
+                               layout.GetStride(),
+                               (const void *)element.Offset);
+        m_VertexBufferIndex++;
+        break;
+      }
+      case ShaderDataType::Mat3:
+      case ShaderDataType::Mat4:
+      {
+        uint8_t count = element.GetComponentCount();
+        for (uint8_t i = 0; i < count; i++)
+        {
+          glEnableVertexAttribArray(m_VertexBufferIndex);
+          glVertexAttribPointer(m_VertexBufferIndex,
+                                count,
+                                ShaderDataTypeToOpenGLBaseType(element.Type),
+                                element.Normalized ? GL_TRUE : GL_FALSE,
+                                layout.GetStride(),
+                                (const void *)(element.Offset + sizeof(float) * count * i));
+          glVertexAttribDivisor(m_VertexBufferIndex, 1);
+          m_VertexBufferIndex++;
+        }
+        break;
+      }
+      default:
+        HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+      }
     }
-    default:
-      HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
-    }
-	}
-	
-	m_VertextBuffers.push_back(vertexBuffer);
-}
 
-void OpenGLVertexArray::SetIndexBuffer(const Ref<IndexBuffer> &indexBuffer)
-{
-	HZ_PROFILE_FUNCTION();
+    m_VertextBuffers.push_back(vertexBuffer);
+  }
 
-	glBindVertexArray(m_RendererID);
-	indexBuffer->Bind();
-	
-	m_IndexBuffer = indexBuffer;
-}
+  void OpenGLVertexArray::SetIndexBuffer(const Ref<IndexBuffer> &indexBuffer)
+  {
+    HZ_PROFILE_FUNCTION();
+
+    glBindVertexArray(m_RendererID);
+    indexBuffer->Bind();
+
+    m_IndexBuffer = indexBuffer;
+  }
 
 }
